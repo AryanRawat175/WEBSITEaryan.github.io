96a25a45c2474dfa90374c38ef67f65b259b7374
AryanRawat175 <99828530+AryanRawat175@users.noreply.github.com>
2023-01-06 23:53:38 +0530

    added website files

---
+++ JS/interest.js
@@ -0,0 +1,266 @@
+$(document).ready(function() {
+    $("h3#one").click(function(q) {
+        q.preventDefault();
+        $("#course1").toggle();
+    });
+});
+$(document).ready(function() {
+    $("h3#two").click(function(q) {
+        q.preventDefault();
+        $("#course2").toggle();
+    });
+});
+$(document).ready(function() {
+    $("h3#three").click(function(q) {
+        q.preventDefault();
+        $("#course3").toggle();
+    });
+});
+$(document).ready(function() {
+    $("h3#four").click(function(q) {
+        q.preventDefault();
+        $("#course4").toggle();
+    });
+});
+$(document).ready(function() {
+    $("h3#five").click(function(q) {
+        q.preventDefault();
+        $("#course5").toggle();
+    });
+});
+$(document).ready(function() {
+    $("h3#six").click(function(q) {
+        q.preventDefault();
+        $("#course6").toggle();
+    });
+});
+$(document).ready(function() {
+    $("button#itA").click(function(q) {
+        q.preventDefault();
+        $("#IT").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#bCs").click(function(q) {
+        q.preventDefault();
+        $("#CS").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#cSe").click(function(q) {
+        q.preventDefault();
+        $("#SE").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#cDs").click(function(q) {
+        q.preventDefault();
+        $("#DS").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#bAc").click(function(q) {
+        q.preventDefault();
+        $("#BAC").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#bAa").click(function(q) {
+        q.preventDefault();
+        $("#BAA").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#fAa").click(function(q) {
+        q.preventDefault();
+        $("#FAA").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#mPe").click(function(q) {
+        q.preventDefault();
+        $("#MPE").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#sIe").click(function(q) {
+        q.preventDefault();
+        $("#SIE").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#vDo").click(function(q) {
+        q.preventDefault();
+        $("#VID").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#pDe").click(function(q) {
+        q.preventDefault();
+        $("#PD").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#hIs").click(function(q) {
+        q.preventDefault();
+        $("#HIS").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#cEc").click(function(q) {
+        q.preventDefault();
+        $("#CEC").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#eCe").click(function(q) {
+        q.preventDefault();
+        $("#ECE").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#mEc").click(function(q) {
+        q.preventDefault();
+        $("#ME").toggle();
+        $("#BA").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#itB").click(function(q) {
+        q.preventDefault();
+        $("#BA").toggle();
+        $("#IT").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#itC").click(function(q) {
+        q.preventDefault();
+        $("#LA").toggle();
+        $("#BA").hide();
+        $("#IT").hide();
+        $("#accounting").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+    $("button#acc").click(function(q) {
+        q.preventDefault();
+        $("#accounting").toggle();
+        $("#BA").hide();
+        $("#IT").hide();
+        $("#LA").hide();
+        $("#HE").hide();
+        $("#MECH").hide();
+    })
+})
+$(document).ready(function() {
+
+    $("button#econ").click(function(q) {
+        q.preventDefault();
+        $("#HE").toggle();
+        $("#BA").hide();
+        $("#IT").hide();
+        $("#LA").hide();
+        $("#accounting").hide();
+        $("#MECH").hide();
+    })
+})
+
+$(document).ready(function() {
+
+    $("button#ENG").click(function(q) {
+        q.preventDefault();
+        $("#MECH").toggle();
+        $("#BA").hide();
+        $("#IT").hide();
+        $("#LA").hide();
+        $("#HE").hide();
+        $("#accounting").hide();
+    })
+})
+
+$(document).ready(function() {
+    $("#btn6").click(function() {
+        alert("ThankYou for your interest, we'll get back at you shortly");
+        window.location.href = "../index.html";
+    })
+})
